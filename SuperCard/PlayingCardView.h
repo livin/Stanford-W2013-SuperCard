77fe255f0b284bcf6f972ad2a61120398a56beff
Vladimir Livin <vlivin@me.com>
2013-05-19 00:46:12 +0400

    Setup initial green background and added single UIView.

---
+++ SuperCard/PlayingCardView.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardView.h
+//  SuperCard
+//
+//  Created by Vladimir on 19.05.13.
+//  Copyright (c) 2013 Vladimir Livin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayingCardView : UIView
+
+@end
